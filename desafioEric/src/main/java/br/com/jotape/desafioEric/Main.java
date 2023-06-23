5caf64891ea227363a08e2e52eb5d999aac2a80c
Jepetossois <132036820+Jepetossois@users.noreply.github.com>
2023-06-23 15:09:55 -0300

    Add files via upload

--- desafioEric/src/main/java/br/com/jotape/desafioEric/Main.java
+++ desafioEric/src/main/java/br/com/jotape/desafioEric/Main.java
@@ -14,34 +14,32 @@ import br.com.jotape.desafioEric.model.entities.Cliente;
 public class Main {
 	public static void main(String[] args) throws IOException {
 
-		int count = 0;
-
 		File json = new File("C:\\1. João Pedro\\JP\\ws - eclipsenew\\desafioEric/cliente.json");
-		ObjectMapper OM = new ObjectMapper();
-		JsonNode JN = OM.readTree(json);
+		ObjectMapper om = new ObjectMapper();
+		JsonNode JN = om.readTree(json);
 		List<Cliente> clientes = new ArrayList<Cliente>();
 		HashSet<Cliente> saida = new HashSet<Cliente>();
 
 		for (JsonNode node : JN) {
+			Cliente c = new Cliente();
+			
 			if (node.has("nome")) {
-				Cliente c = new Cliente(node.get("nome").asText(), node.get("entrada").asText());
-				clientes.add(c);
-
+				c = new Cliente(node.get("nome").asText(), node.get("entrada").asText());
+				
 			} else if (node.has("entrada")) {
 				JsonNode entrada = node.get("entrada");
-				Cliente c = new Cliente(entrada.get("nome").asText(), entrada.get("idCliente").asText());
-				clientes.add(c);
-
+				c = new Cliente(entrada.get("nome").asText(), entrada.get("idCliente").asText());
+				
 			}
+			clientes.add(c);
 		}
 
+		int count = 0;
 		for (Cliente c : clientes) {
-
 			if (!saida.contains(c)) {
 				System.out.println(c.getIdCliente());
 				saida.add(c);
 				count++;
-
 			}
 		}
 
